0fdedfda696010aa809e6cc1852b5edca240a45f
mika.urakawa <awakaru0122@gmail.com>
2024-01-05 16:07:03 -0800

    delete: unnecessary comments

--- Package.swift
+++ Package.swift
@@ -9,18 +9,14 @@ let package = Package(
         .iOS(.v16),
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "AppInfoList",
             targets: ["AppInfoList"]),
     ],
     dependencies: [
-//         Dependencies declare other packages that this package depends on.
          .package(url: "https://github.com/cybozu/LicenseList.git", from: "0.2.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "AppInfoList",
             dependencies: [
